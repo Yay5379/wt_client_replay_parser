7b89fd4b491ed02a4558bdbee251f2f9288525a9
Yay5379 <61430523+Yay5379@users.noreply.github.com>
2024-03-16 15:03:25 -0500

    Update cx_setup.py

--- cx_setup.py
+++ cx_setup.py
@@ -2,7 +2,7 @@ import os.path
 import sys
 from cx_Freeze import setup, Executable
 
-src_path = "wt_client_replay_parser/src/"
+src_path = "src/wt_client_replay_parser/"
 packages = ["multiprocessing"]
 includes = []
 excludes = ["unittest", "pydoc", "construct.examples", "bz2", "lib2to3", "test", "tkinter"]
