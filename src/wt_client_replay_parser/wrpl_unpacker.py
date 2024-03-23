771b9a02cfefa5e98ae3bde90710e3d35f317203
Yay5379 <61430523+Yay5379@users.noreply.github.com>
2024-03-23 15:03:09 -0500

    Add files via upload

--- src/wt_client_replay_parser/wrpl_unpacker.py
+++ src/wt_client_replay_parser/wrpl_unpacker.py
@@ -1,17 +1,3 @@
-"""
-Распаковка клиентского реплея.
-
-Вход:
-#2021.09.20 19.30.33.wrpl
-
-Выход:
-#2021.09.20 19.30.33.wrpl.d/
-├── m_set.blkx
-├── rez.blkx
-├── info.json
-└── wrplu.bin
-"""
-
 import argparse
 import json
 from pathlib import Path
@@ -21,7 +7,6 @@ import sys
 import construct as ct
 from blk.types import Section
 import blk.text as txt
-import blk.json as jsn
 try:
     from formats.wrpl_parser import WRPLCliFile
 except ImportError:
@@ -35,28 +20,8 @@ try:
 except ImportError:
     from formats.parse_replay import parse_replay
 
-STRICT_BLK = 'strict_blk'
-JSON = 'json'
-JSON_2 = 'json_2'
-JSON_3 = 'json_3'
-
-out_type_map = {
-    STRICT_BLK: txt.STRICT_BLK,
-    JSON: jsn.JSON,
-    JSON_2: jsn.JSON_2,
-    JSON_3: jsn.JSON_3,
-}
-
-
-def suffix(out_format: str) -> str:
-    return '.blkx' if out_format == STRICT_BLK else '.json'
-
-
-def serialize_text(root: Section, ostream: t.TextIO, out_type: int, is_sorted: bool = False):
-    if out_type == txt.STRICT_BLK:
-        txt.serialize(root, ostream, dialect=txt.StrictDialect)
-    elif out_type in (jsn.JSON, jsn.JSON_2, jsn.JSON_3):
-        jsn.serialize(root, ostream, out_type, is_sorted)
+def serialize_text(root: Section, ostream: t.TextIO):
+    txt.serialize(root, ostream, dialect=txt.StrictDialect)
 
 
 def create_text(path: os.PathLike) -> t.TextIO:
@@ -68,12 +33,8 @@ def main():
     parser.add_argument('replay', type=argparse.FileType('rb'), help='Файл реплея.')
     parser.add_argument('-o', dest='out_dir', type=Path, default=Path.cwd(),
                         help='Выходная директория. По умолчанию %(default)s.')
-    parser.add_argument('--format', dest='out_format', choices=list(out_type_map), default=STRICT_BLK,
-                        help='Формат для blk. По умолчанию %(default)s.')
     ns = parser.parse_args()
     replay = ns.replay
-    out_format = ns.out_format
-    out_type = out_type_map[out_format]
     replay_path = Path(replay.name)
     out_dir: Path = ns.out_dir / f'{replay_path.name}.d'
 
@@ -91,9 +52,9 @@ def main():
 
     for name in ('m_set', 'rez'):
         section = parsed[name]
-        out_path = (out_dir / name).with_suffix(suffix(out_format))
+        out_path = (out_dir / name).with_suffix('.blk')
         with create_text(out_path) as ostream:
-            serialize_text(section, ostream, out_type)
+            serialize_text(section, ostream)
 
     out_path = out_dir / 'wrplu.bin'
     out_path.write_bytes(parsed.wrplu)
